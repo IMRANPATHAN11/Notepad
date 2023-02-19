0f35f510a8da64e8f12facf1fd5160e21ce3feed
imran khan <imranpathanik111@gmail.com>
2023-02-19 14:59:22 +0530

    Add project files.

---
+++ Notepad/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog imran = new OpenFileDialog();
+            imran.Title = "Open";
+            imran.Filter = "Text Dacument(*.txt)|*.txt|All Files(*.*)|*.*";
+            if (imran.ShowDialog() == DialogResult.OK)
+                richTextBox1.LoadFile(imran.FileName, RichTextBoxStreamType.PlainText);
+            this.Text = imran.FileName;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog imran = new SaveFileDialog();
+            imran.Title = "Save";
+            imran.Filter = "Text Dacument(*.txt)|*.txt|All Files(*.*)|*.*";
+            if (imran.ShowDialog() == DialogResult.OK)
+                richTextBox1.SaveFile(imran.FileName, RichTextBoxStreamType.PlainText);
+            this.Text = imran.FileName;
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void dateTimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = System.DateTime.Now.ToString();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fnt = new FontDialog();
+            if (fnt.ShowDialog() == DialogResult.OK)
+                richTextBox1.Font = fnt.Font;
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog fnt = new ColorDialog();
+            if (fnt.ShowDialog() == DialogResult.OK)
+                richTextBox1.ForeColor = fnt.Color;
+        }
+    }
+}
